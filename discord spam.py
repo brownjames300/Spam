3a3c96c0de4452d316531d35760d13375f0fa1c3
James Brown <brownjames6571@gmail.com>
2020-12-27 20:17:36 +0000

    Update discord spam.py

--- discord spam.py
+++ discord spam.py
@@ -1,5 +1,5 @@
 import pyautogui, time
 f = open("bee movie script.txt", "r")
 for word in f:
-    pyautogui.typewrite(f"@CoolTomato {word}")
+    pyautogui.typewrite(word)
     pyautogui.press("enter")
